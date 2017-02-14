c4ad074029dc18b90a979bc341bf768d91bf323f
Nicolas Muntwyler <nicolas.muntwyler@gmail.com>
2017-02-14 17:41:22 +0100

    on ui everything works, but when i look at the state in devTool the state doesnt look good

--- src/reducers/tales.js
+++ src/reducers/tales.js
@@ -10,77 +10,98 @@ const initialState = {
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
   ],
-  count: 1,
+  count: 0,
 };
-
-function setSymbol(tempStore, action) {
-  console.log(tempStore.count);
-  if (tempStore.count === 0) {
-    tempStore = {
-      ...tempStore,
-      cells: [
-        ...tempStore.cells.slice(0, action.index),
-        tempStore.cells[action.index] = 'X',
-        ...tempStore.cells.slice(action.index + 1),
-      ],
-      count: 1,
-    };
-  } else {
-    tempStore = {
-      ...tempStore,
-      cells: [
-        ...tempStore.cells.slice(0, action.index),
-        tempStore.cells[action.index] = 'O',
-        ...tempStore.cells.slice(action.index + 1),
-      ],
-      count: 0,
-    };
-  }
-}
-
-function kickSymbol(tempStore, action) {
-  let id = action.index;
-  while (tempStore.cells[id] === 'X' || tempStore.cells[id] === 'O') {
-    if (id > 6) {
-      tempStore = {
-        ...tempStore,
-        cells: [
-          ...tempStore.cells.slice(0, id),
-          tempStore.cells[id] = tempStore.cells[id - 7],
-          ...tempStore.cells.slice(id + 1),
-        ],
-      };
-    } else {
-      tempStore = {
-        ...tempStore,
-        cells: [
-          ...tempStore.cells.slice(0, id),
-          tempStore.cells[id] = ' ',
-          ...tempStore.cells.slice(id + 1),
-        ],
-      };
-    }
-    id -= 7;
-  }
-}
 
 const tale = (state = initialState, action) => {
   switch (action.type) {
     case 'CLICK_BUTTON': {
       const index = action.index;
-      const tempStore = { ...state };
-
-      console.log(tempStore);
+      let tempStore = state;
 
       if (tempStore.cells[index] === 'X' || tempStore.cells[index] === 'O') {
-        kickSymbol(tempStore, action);
+        // Kick the Symbol
+        let id = action.index;
+        while (tempStore.cells[id] === 'X' || tempStore.cells[id] === 'O') {
+          // Rows 1-6
+          if (id > 6) {
+            tempStore = {
+              ...tempStore,
+              cells: [
+                ...tempStore.cells.slice(0, id),
+                // tempStore.cells[id] = 'l',
+                tempStore.cells[id] = tempStore.cells[id - 7],
+                ...tempStore.cells.slice(id + 1),
+              ],
+            };
+          } else {
+            // Row 7
+            tempStore = {
+              ...tempStore,
+              cells: [
+                ...tempStore.cells.slice(0, id),
+                tempStore.cells[id] = ' ',
+                ...tempStore.cells.slice(id + 1),
+              ],
+            };
+          }
+          id -= 7;
+        }
       } else if (tempStore.cells[index] === ' ') {
+        // Place a Symbol
         if (index > 34) {
-          setSymbol(tempStore, action);
+          // Row 1
+          if (tempStore.count === 0) {
+            // Place X
+            tempStore = {
+              ...tempStore,
+              cells: [
+                ...tempStore.cells.slice(0, action.index),
+                tempStore.cells[action.index] = 'X',
+                ...tempStore.cells.slice(action.index + 1),
+              ],
+              count: 1,
+            };
+          } else {
+            // Place O
+            tempStore = {
+              ...tempStore,
+              cells: [
+                ...tempStore.cells.slice(0, action.index),
+                tempStore.cells[action.index] = 'O',
+                ...tempStore.cells.slice(action.index + 1),
+              ],
+              count: 0,
+            };
+          }
         } else if (tempStore.cells[index + 7] !== ' ') {
-          setSymbol(tempStore, action);
+          // Row 2-7
+          if (tempStore.count === 0) {
+            // Place X
+            tempStore = {
+              ...tempStore,
+              cells: [
+                ...tempStore.cells.slice(0, action.index),
+                tempStore.cells[action.index] = 'X',
+                ...tempStore.cells.slice(action.index + 1),
+              ],
+              count: 1,
+            };
+          } else {
+            // Place O
+            tempStore = {
+              ...tempStore,
+              cells: [
+                ...tempStore.cells.slice(0, action.index),
+                tempStore.cells[action.index] = 'O',
+                ...tempStore.cells.slice(action.index + 1),
+              ],
+              count: 0,
+            };
+          }
         }
       }
+      console.log('2', tempStore);
       return tempStore;
     }
     default: {
