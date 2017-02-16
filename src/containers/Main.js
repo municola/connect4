b1f7c7d543d6a87eb1fc5219949d9c83f5bb17de
Nicolas Muntwyler <nicolas.muntwyler@gmail.com>
2017-02-16 16:44:50 +0100

    logic is in reducer, but doesnt work

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -27,62 +27,16 @@ const style = {
 
 class App extends Component {
   getButtons() {
-    return this.props.tale.cells.map((tales, index) => {
+    console.log(this.props.tale.get(1));
+    return this.props.tale.get(1).map((tales, index) => {
       return (
         <button
           onClick={() => this.props.clickButton(index)}
           style={style.button}
           key={index}
-        >{this.props.tale.cells[index]}</button>
+        >{this.props.tale.get(1)[index]}</button>
       );
     });
-  }
-
-  setSymbol(index) {
-    if (this.props.tale.count === 0) {
-      this.props.setX(index);
-    } else {
-      this.props.setO(index);
-    }
-  }
-
-  handleOnClick(index) {
-    if (this.props.tale.cells[index] === 'X' || this.props.tale.cells[index] === 'O') {
-      this.checkForState2(index);
-      // this.props.getState();
-    }
-    else if (this.props.tale.cells[index] === ' ') {
-      if (index > 34) {
-        this.checkForState(index);
-      }
-      if (this.props.tale.cells[index + 7] !== ' ') {
-        this.checkForState(index);
-      }
-    }
-  }
-
-  checkForState(index) {
-    this.setSymbol(index);
-  }
-
-  checkForState2(index) {
-    let id = index;
-    while (this.props.tale.cells[id] === 'X' || this.props.tale.cells[id] === 'O') {
-      if (id > 6) {
-        this.props.cickSymbol(id);
-      } else {
-        this.props.cickSymbol2(id);
-      }
-      id -= 7;
-    }
-    for (let l = 0; l < this.props.talePast.length; l++) {
-      if (this.props.tale.cells === this.props.talePast[l]) {
-        // this.props.tale.cells = this.props.talePast[l].cells;
-        // (geht nicht, da store read-only)
-        // => {this.props.onUndo} ??
-        // Wird messy
-      }
-    }
   }
 
   render() {
--- src/reducers/tales.js
+++ src/reducers/tales.js
@@ -1,8 +1,10 @@
 import undoable from 'redux-undo';
 
-const initialState = {
-  winner: false,
-  cells: [
+const { List } = require('immutable');
+
+const initialState = List.of(
+  [false],
+  [
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
@@ -10,98 +12,54 @@ const initialState = {
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
     ' ', ' ', ' ', ' ', ' ', ' ', ' ',
   ],
-  count: 0,
-};
+  [0],
+);
 
 const tale = (state = initialState, action) => {
   switch (action.type) {
     case 'CLICK_BUTTON': {
       const index = action.index;
-      let tempStore = { ...state };
+      let tempStore = state;
 
-      if (tempStore.cells[index] === 'X' || tempStore.cells[index] === 'O') {
+      if (tempStore.get(1)[index] === 'X' || tempStore.get(1)[index] === 'O') {
         // Kick the Symbol
         let id = action.index;
-        while (tempStore.cells[id] === 'X' || tempStore.cells[id] === 'O') {
+        while (tempStore.get(1)[id] === 'X' || tempStore.get(1)[id] === 'O') {
           // Rows 1-6
           if (id > 6) {
-            tempStore = {
-              ...tempStore,
-              cells: [
-                ...tempStore.cells.slice(0, id),
-                // tempStore.cells[id] = 'l',
-                tempStore.cells[id] = tempStore.cells[id - 7],
-                ...tempStore.cells.slice(id + 1),
-              ],
-            };
+            tempStore.set(tempStore.get(1)[id], tempStore.get(1)[id - 7]);
           } else {
             // Row 7
-            tempStore = {
-              ...tempStore,
-              cells: [
-                ...tempStore.cells.slice(0, id),
-                tempStore.cells[id] = ' ',
-                ...tempStore.cells.slice(id + 1),
-              ],
-            };
+            tempStore.set(tempStore.get(1)[id], ' ');
           }
           id -= 7;
         }
-      } else if (tempStore.cells[index] === ' ') {
+      } else if (tempStore.get(1)[index] === ' ') {
         // Place a Symbol
         if (index > 34) {
           // Row 1
-          if (tempStore.count === 0) {
+          if (tempStore.get(2)[0] === 0) {
             // Place X
-            tempStore = {
-              ...tempStore,
-              cells: [
-                ...tempStore.cells.slice(0, action.index),
-                tempStore.cells[action.index] = 'X',
-                ...tempStore.cells.slice(action.index + 1),
-              ],
-              count: 1,
-            };
+            tempStore.set(tempStore.get(1)[action.index], 'X');
+            tempStore.set(tempStore.get(2)[0], 1);
           } else {
             // Place O
-            tempStore = {
-              ...tempStore,
-              cells: [
-                ...tempStore.cells.slice(0, action.index),
-                tempStore.cells[action.index] = 'O',
-                ...tempStore.cells.slice(action.index + 1),
-              ],
-              count: 0,
-            };
+            tempStore.set(tempStore.get(1)[action.index], 'O');
+            tempStore.set(tempStore.get(2)[0], 0);
           }
-        } else if (tempStore.cells[index + 7] !== ' ') {
+        } else if (tempStore.get(1)[index + 7] !== ' ') {
           // Row 2-7
-          if (tempStore.count === 0) {
+          if (tempStore.get(2)[0] === 0) {
             // Place X
-            tempStore = {
-              ...tempStore,
-              cells: [
-                ...tempStore.cells.slice(0, action.index),
-                tempStore.cells[action.index] = 'X',
-                ...tempStore.cells.slice(action.index + 1),
-              ],
-              count: 1,
-            };
+            tempStore.set(tempStore.get(1)[action.index], 'X');
+            tempStore.set(tempStore.get(2)[0], 1);
           } else {
             // Place O
-            tempStore = {
-              ...tempStore,
-              cells: [
-                ...tempStore.cells.slice(0, action.index),
-                tempStore.cells[action.index] = 'O',
-                ...tempStore.cells.slice(action.index + 1),
-              ],
-              count: 0,
-            };
+            tempStore.set(tempStore.get(1)[action.index], 'O');
+            tempStore.set(tempStore.get(2)[0], 0);
           }
         }
       }
-      console.log(tempStore.cells);
       return tempStore;
     }
     default: {
